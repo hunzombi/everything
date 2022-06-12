42d33c55b14344d7b7f6ff98248de1c0e96581c4
hunzombi <107367582+hunzombi@users.noreply.github.com>
2022-06-12 22:24:00 +0300

    Update basic.py

--- basic.py
+++ basic.py
@@ -5,3 +5,18 @@ class Vector:
   
   def add_vector(self, vec):
     return Vector(self.x + vec.x, self.y + vec.y)
+
+def pythagoras(a, b):
+  return ((a**2)+(b**2))**0.5
+
+def sqrt(num):
+  return num**0.5
+
+def listtostring(arr):
+  return ''.join(str(i) for i in arr)
+
+def prodoflist(arr):
+  res = 1
+  for i in arr:
+    res *= i
+  return res else 0
